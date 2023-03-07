05379eb5ec7d2d659e34a02fca5f9a0d61ca5bcf
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2023-03-07 14:59:22 +0600

    docs: add help and about info for app and with cmd

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,20 @@ use coauthor_repo::{CoauthorRepo, GitConfigCoauthorRepo};
 use handlers::with;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    author = "Mubashwer Salman Khurshid",
+    version,
+    about = "A CLI app to help you automatically add co-author(s) to commits
+during pair/mob programming sessions.",
+    long_about = "
+You can attribute a git commit to more than one author by adding one or more
+Co-authored-by trailers to the commit's message. 
+Co-authored commits are visible on GitHub.
+
+This CLI app will make it easy for you to manage pair/mob programming sessions
+and automatically add Co-authored-by trailers to the commit's message.
+"
+)]
 #[command(propagate_version = true)]
 struct Cli {
     #[command(subcommand)]
@@ -17,7 +30,25 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    With { coauthor_keys: Option<Vec<String>> },
+    #[command(
+        about = "Sets active co-author(s) for the pair/mob programming session",
+        long_about = "
+Sets active co-author(s) for the pair/mob programming session.
+This should be run after at least one co-author(s) has been added.
+
+If no COAUTHOR_KEY arg is provided, a multi-select prompt will be displayed to
+aid selection from available co-authors.
+
+If COAUTHOR_KEY(s) are provided, co-author(s) must exist with the given key(s).
+
+Example usage:
+$ git pair with lm cr
+"
+    )]
+    With {
+        #[arg(help = "Keys used to store co-authors (usually initials).")]
+        coauthor_keys: Option<Vec<String>>,
+    },
 }
 
 fn main() {
