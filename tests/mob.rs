4db86132976afc3759dfbc943059af25c5e5c78e
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2023-04-07 22:48:32 +1000

    test: remove testing commit output in -w and -c integration tests

--- tests/mob.rs
+++ tests/mob.rs
@@ -1,7 +1,7 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use serial_test::serial;
-use std::{env, process::Command};
+use std::process::Command;
 
 fn before_each() -> Result<(), Box<dyn std::error::Error>> {
     Command::new("git")
@@ -35,43 +35,7 @@ fn before_each() -> Result<(), Box<dyn std::error::Error>> {
     ]);
     cmd.assert().success();
 
-    // set global githooks directory to current directory
-    // so it can find prepare-commit-msg
-    let current_dir = env::current_dir()?.display().to_string();
-    Command::new("git")
-        .args(["config", "--global", "core.hooksPath", &current_dir])
-        .output()
-        .expect("failed to execute process");
-
     Ok(())
-}
-
-fn assert_commit_message(input_msg: &str, expected_msg: &str) {
-    let mut cmd = Command::new("git");
-    cmd.args(["config", "user.email", "test@email.com"]);
-    cmd.assert().success();
-
-    let mut cmd = Command::new("git");
-    cmd.args(["config", "user.name", "Test User"]);
-    cmd.assert().success();
-    
-    
-    // making a test commit
-    let mut cmd = Command::new("git");
-    cmd.args(["commit", "-m", input_msg, "--allow-empty"]);
-    cmd.assert().success();
-
-    // asserting test commit has the expected message
-    // note: git show displays the commit message with an indent of 4 spaces
-    cmd = Command::new("git");
-    cmd.args(["show", "--name-only"]);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::ends_with(expected_msg));
-
-    cmd = Command::new("git");
-    cmd.args(["reset", "--soft", "HEAD~1"]);
-    cmd.assert().success();
 }
 
 #[test]
@@ -92,16 +56,6 @@ fn test_mob_with_by_keys() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert().success().stdout(predicate::str::diff(
         "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
     ));
-
-    let input_commit_msg = "making a test commit";
-    let expected_commit_msg = r#"
-    making a test commit
-    
-    Co-authored-by: Leo Messi <leo.messi@example.com>
-    Co-authored-by: Emi Martinez <emi.martinez@example.com>
-"#;
-
-    assert_commit_message(input_commit_msg, expected_commit_msg);
 
     Ok(())
 }
@@ -127,13 +81,6 @@ fn test_mob_clear() -> Result<(), Box<dyn std::error::Error>> {
     cmd = Command::cargo_bin("git-mob")?;
     cmd.args(["--list"]);
     cmd.assert().success().stdout(predicate::str::diff(""));
-
-    let input_commit_msg = "making a test commit";
-    let expected_commit_msg = r#"
-    making a test commit
-"#;
-
-    assert_commit_message(input_commit_msg, expected_commit_msg);
 
     Ok(())
 }
