794b6e0b3d4823414ce1b9eb5eb7575d8c0273d3
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2024-03-20 01:53:10 +1100

    test: remove redundant cmd variables

--- tests/coauthor.rs
+++ tests/coauthor.rs
@@ -16,36 +16,40 @@ fn test_add_coauthors() -> Result<(), Box<dyn std::error::Error>> {
     before_each();
 
     // adding 2 co-authors
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "lm",
-        "Leo Messi",
-        "leo.messi@example.com",
-    ]);
-    cmd.assert()
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "lm",
+            "Leo Messi",
+            "leo.messi@example.com",
+        ])
+        .assert()
         .success()
         .stdout(predicate::str::diff("Leo Messi <leo.messi@example.com>\n"));
 
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "em",
-        "Emi Martinez",
-        "emi.martinez@example.com",
-    ]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "Emi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "em",
+            "Emi Martinez",
+            "emi.martinez@example.com",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Emi Martinez <emi.martinez@example.com>\n",
+        ));
 
     // co-authors list shows the 2 co-authors that were added
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["coauthor", "--list"]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "lm Leo Messi <leo.messi@example.com>\nem Emi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args(["coauthor", "--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "lm Leo Messi <leo.messi@example.com>\nem Emi Martinez <emi.martinez@example.com>\n",
+        ));
 
     Ok(())
 }
@@ -56,37 +60,42 @@ fn test_delete_coauthor() -> Result<(), Box<dyn std::error::Error>> {
     before_each();
 
     // adding 2 co-authors
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "lm",
-        "Leo Messi",
-        "leo.messi@example.com",
-    ]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "lm",
+            "Leo Messi",
+            "leo.messi@example.com",
+        ])
+        .assert()
+        .success();
 
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "em",
-        "Emi Martinez",
-        "emi.martinez@example.com",
-    ]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "em",
+            "Emi Martinez",
+            "emi.martinez@example.com",
+        ])
+        .assert()
+        .success();
 
     // deleting one co-author
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["coauthor", "--delete", "lm"]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args(["coauthor", "--delete", "lm"])
+        .assert()
+        .success();
 
     // co-authors list excludes the deleted co-author
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["coauthor", "--list"]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "em Emi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args(["coauthor", "--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "em Emi Martinez <emi.martinez@example.com>\n",
+        ));
 
     Ok(())
 }
--- tests/mob.rs
+++ tests/mob.rs
@@ -15,25 +15,27 @@ fn before_each() -> Result<(), Box<dyn std::error::Error>> {
         .expect("failed to execute process");
 
     // adding 2 co-authors
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "lm",
-        "Leo Messi",
-        "leo.messi@example.com",
-    ]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "lm",
+            "Leo Messi",
+            "leo.messi@example.com",
+        ])
+        .assert()
+        .success();
 
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args([
-        "coauthor",
-        "--add",
-        "em",
-        "Emi Martinez",
-        "emi.martinez@example.com",
-    ]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args([
+            "coauthor",
+            "--add",
+            "em",
+            "Emi Martinez",
+            "emi.martinez@example.com",
+        ])
+        .assert()
+        .success();
 
     Ok(())
 }
@@ -44,18 +46,22 @@ fn test_mob_with_by_keys() -> Result<(), Box<dyn std::error::Error>> {
     before_each()?;
 
     // mobbing with both of the co-authors
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["--with", "lm", "em"]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args(["--with", "lm", "em"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
 
     // verifying mob list shows both of the co-authors
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["--list"]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args(["--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
 
     Ok(())
 }
@@ -66,21 +72,26 @@ fn test_mob_clear() -> Result<(), Box<dyn std::error::Error>> {
     before_each()?;
 
     // mobbing with both of the co-authors
-    let mut cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["--with", "lm", "em"]);
-    cmd.assert().success().stdout(predicate::str::diff(
-        "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
-    ));
+    Command::cargo_bin("git-mob")?
+        .args(["--with", "lm", "em"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
 
     // clearing mob session
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["--clear"]);
-    cmd.assert().success();
+    Command::cargo_bin("git-mob")?
+        .args(["--clear"])
+        .assert()
+        .success();
 
     // verifying mob list is empty
-    cmd = Command::cargo_bin("git-mob")?;
-    cmd.args(["--list"]);
-    cmd.assert().success().stdout(predicate::str::diff(""));
+    Command::cargo_bin("git-mob")?
+        .args(["--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(""));
 
     Ok(())
 }
