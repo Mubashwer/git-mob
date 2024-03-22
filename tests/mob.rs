7f291157cc8c6a8da35b2cfacd41983c779608d3
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2024-03-23 02:22:25 +1100

    test: add tests for git mob --with prompt
    Add integration tests for git mob --with multiselect prompt command
    
    The tests are a bit flaky when parallel, so I had to use
    serial-test. Will debug this another day

--- tests/mob.rs
+++ tests/mob.rs
@@ -1,13 +1,14 @@
 mod helpers;
 
+use serial_test::serial;
 use assert_cmd::prelude::*;
 use helpers::test_contexts::TestContextCli;
 use predicates::prelude::*;
+use rexpect::session::spawn_command;
 use std::error::Error;
 use test_context::test_context;
 
-fn before_each(ctx: &TestContextCli) -> Result<(), Box<dyn Error>> {
-    // adding 2 co-authors
+fn add_two_coauthors(ctx: &TestContextCli) -> Result<(), Box<dyn Error>> {
     ctx.git()
         .args([
             "mob",
@@ -38,7 +39,7 @@ fn before_each(ctx: &TestContextCli) -> Result<(), Box<dyn Error>> {
 #[test_context(TestContextCli, skip_teardown)]
 #[test]
 fn test_mob_with_by_keys(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
-    before_each(&ctx)?;
+    add_two_coauthors(&ctx)?;
 
     // mobbing with both of the co-authors
     ctx.git()
@@ -49,7 +50,133 @@ fn test_mob_with_by_keys(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
             "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
         ));
 
-    // verifying mob list shows both of the co-authors
+    // verifying mob session has both of the selected co-authors
+    ctx.git()
+        .args(["mob", "--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
+
+    Ok(())
+}
+
+#[test_context(TestContextCli, skip_teardown)]
+#[test]
+#[serial]
+fn test_mob_with_multiselect_given_no_coauthors(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
+    // running command to display mob session multiselect prompt
+    let mut command = ctx.git();
+    command.args(["mob", "--with"]);
+
+    let mut session = spawn_command(command, Some(5000))?;
+    session.exp_string("No co-author(s) found. At least one co-author must be added")?;
+    session.process.wait()?;
+
+    Ok(())
+}
+
+#[test_context(TestContextCli, skip_teardown)]
+#[test]
+#[serial]
+fn test_mob_with_multiselect_select_none(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
+    // given a mob session with 2 co-authors is active
+    add_two_coauthors(&ctx)?;
+    ctx.git()
+        .args(["mob", "--with", "lm", "em"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
+
+    // running command to display mob session multiselect prompt
+    let mut command = ctx.git();
+    command.args(["mob", "--with"]);
+
+    let mut session = spawn_command(command, Some(5000))?;
+    session.exp_string("Select active co-author(s)")?;
+    session.exp_string("Leo Messi <leo.messi@example.com>")?;
+    session.exp_string("Emi Martinez <emi.martinez@example.com>")?;
+
+    // pressing enter to end prompt without selecting any co-authors
+    session.send_control('m')?;
+    session.exp_string("Going solo!")?;
+    session.process.wait()?;
+
+    // verifying mob session is now empty
+    ctx.git()
+        .args(["mob", "--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(""));
+
+    Ok(())
+}
+
+#[test_context(TestContextCli, skip_teardown)]
+#[test]
+#[serial]
+fn test_mob_with_multiselect_select_coauthor(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
+    add_two_coauthors(&ctx)?;
+
+    // running command to display mob session multiselect prompt
+    let mut command = ctx.git();
+    command.args(["mob", "--with"]);
+
+    let mut session = spawn_command(command, Some(5000))?;
+    session.exp_string("Select active co-author(s)")?;
+    session.exp_string("Leo Messi <leo.messi@example.com>")?;
+    session.exp_string("Emi Martinez <emi.martinez@example.com>")?;
+
+    // pressing space to select first co-author
+    session.send(" ")?;
+    session.flush()?;
+
+    // pressing enter to end prompt
+    session.send_control('m')?;
+    session.process.wait()?;
+
+    // verifying mob session has selected co-author
+    ctx.git()
+        .args(["mob", "--list"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff("Leo Messi <leo.messi@example.com>\n"));
+
+    Ok(())
+}
+
+#[test_context(TestContextCli, skip_teardown)]
+#[test]
+#[serial]
+fn test_mob_with_multiselect_escape(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
+    // given a mob session  with 2 co-authors is active
+    add_two_coauthors(&ctx)?;
+    ctx.git()
+        .args(["mob", "--with", "lm", "em"])
+        .assert()
+        .success()
+        .stdout(predicate::str::diff(
+            "Leo Messi <leo.messi@example.com>\nEmi Martinez <emi.martinez@example.com>\n",
+        ));
+
+    // running command to display mob session multiselect prompt
+    let mut command = ctx.git();
+    command.args(["mob", "--with"]);
+
+    let mut session = spawn_command(command, Some(5000))?;
+    session.exp_string("Select active co-author(s)")?;
+    session.exp_string("Leo Messi <leo.messi@example.com>")?;
+    session.exp_string("Emi Martinez <emi.martinez@example.com>")?;
+
+    // pressing escape to cancel prompt
+    session.send_control('[')?;
+    session.exp_string("<canceled>")?;
+    session.process.wait()?;
+
+    // verifying mob list shows selected co-authors are unchanged
     ctx.git()
         .args(["mob", "--list"])
         .assert()
@@ -64,7 +191,7 @@ fn test_mob_with_by_keys(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
 #[test_context(TestContextCli, skip_teardown)]
 #[test]
 fn test_mob_clear(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
-    before_each(&ctx)?;
+    add_two_coauthors(&ctx)?;
 
     // mobbing with both of the co-authors
     ctx.git()
@@ -91,7 +218,7 @@ fn test_mob_clear(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
 #[test_context(TestContextCli, skip_teardown)]
 #[test]
 fn test_mob_coauthor_trailers(ctx: TestContextCli) -> Result<(), Box<dyn Error>> {
-    before_each(&ctx)?;
+    add_two_coauthors(&ctx)?;
 
     // mobbing with both of the co-authors
     ctx.git()
